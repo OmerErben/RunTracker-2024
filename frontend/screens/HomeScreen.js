0e8ea5451e718b0664f4fd1a8678b04a8df84fe4
omererben <omer.erben@gmail.com>
2024-07-06 14:27:48 +0300

    Added sign up option

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -4,95 +4,97 @@ import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 
 const HomeScreen = () => {
-  const [location, setLocation] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [integerInput, setIntegerInput] = useState('');
+    const [location, setLocation] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [integerInput, setIntegerInput] = useState('');
 
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setLocation(null);
-        setLoading(false);
-        return;
-      }
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setLocation(null);
+                setLoading(false);
+                return;
+            }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location.coords);
-      setLoading(false);
-    })();
-  }, []);
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location.coords);
+            setLoading(false);
+        })();
+    }, []);
 
-  const handleInputChange = (text) => {
-    // Ensure the input is an integer
-    if (/^\d*$/.test(text)) {
-      setIntegerInput(text);
+    const handleInputChange = (text) => {
+        // Ensure the input is an integer
+        if (/^\d*$/.test(text)) {
+            setIntegerInput(text);
+        }
+    };
+
+    if (loading) {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size="large" color="#6200ee" />
+            </View>
+        );
     }
-  };
 
-  if (loading) {
     return (
         <View style={styles.container}>
-          <ActivityIndicator size="large" color="#6200ee" />
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: location ? location.latitude : 37.78825,
+                    longitude: location ? location.longitude : -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            >
+                {location && (
+                    <Marker
+                        coordinate={{
+                            latitude: location.latitude,
+                            longitude: location.longitude,
+                        }}
+                        title="Your Location"
+                    />
+                )}
+            </MapView>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Please select a radius in meters"
+                    value={integerInput}
+                    onChangeText={handleInputChange}
+                    keyboardType="numeric"
+                />
+            </View>
         </View>
     );
-  }
-
-  return (
-      <View style={styles.container}>
-        <MapView
-            style={styles.map}
-            initialRegion={{
-              latitude: location ? location.latitude : 37.78825,
-              longitude: location ? location.longitude : -122.4324,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-        >
-          {location && (
-              <Marker
-                  coordinate={{
-                    latitude: location.latitude,
-                    longitude: location.longitude,
-                  }}
-                  title="Your Location"
-              />
-          )}
-        </MapView>
-        <TextInput
-            style={styles.input}
-            placeholder="Please select a radius in meters"
-            value={integerInput}
-            onChangeText={handleInputChange}
-            keyboardType="numeric"
-        />
-      </View>
-  );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  input: {
-    position: 'absolute',
-    top: 40,
-    right: 10,
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    paddingHorizontal: 8,
-    borderRadius: 4,
-    width: 250,
-    backgroundColor: '#fff',
-  },
-  errorText: {
-    fontSize: 18,
-    color: 'red',
-  },
+    container: {
+        flex: 1,
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+    inputContainer: {
+        position: 'absolute',
+        top: 20,
+        left: 10,
+        right: 10,
+        alignItems: 'center',
+    },
+    input: {
+        height: 40,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        paddingHorizontal: 8,
+        borderRadius: 4,
+        width: '100%',
+        backgroundColor: '#fff',
+    },
 });
 
 export default HomeScreen;
--- frontend/screens/LoginScreen.js
+++ frontend/screens/LoginScreen.js
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
-import {Snackbar} from "react-native-paper";
-
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { Snackbar } from "react-native-paper";
+import { Ionicons } from '@expo/vector-icons'; // Import Ionicons from Expo
 
 const LoginScreen = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [visible, setVisible] = useState(false);
+    const [secureTextEntry, setSecureTextEntry] = useState(true); // State for password visibility
 
     const handleLogin = () => {
-        // Todo
+        // Todo: Add real db
         if (email === 'user@example.com' && password === 'password') {
             navigation.navigate('Home');
         } else {
             setVisible(true);
         }
+    };
+
+    const handleSignUp = () => {
+        // Todo: Add real option to sign up
+        console.log("does nothing");
+    };
+
+    const toggleSecureEntry = () => {
+        setSecureTextEntry(!secureTextEntry);
     };
 
     return (
@@ -28,14 +38,26 @@ const LoginScreen = ({ navigation }) => {
                 keyboardType="email-address"
                 autoCapitalize="none"
             />
-            <TextInput
-                style={styles.input}
-                placeholder="Password"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-            />
-            <Button title="Login" onPress={handleLogin} />
+            <View style={styles.passwordContainer}>
+                <TextInput
+                    style={styles.passwordInput}
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry={secureTextEntry}
+                />
+                <TouchableOpacity onPress={toggleSecureEntry} style={styles.eyeIcon}>
+                    <Ionicons name={secureTextEntry ? 'eye-off-outline' : 'eye-outline'} size={24} color="black" />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.buttonContainer}>
+                <View style={styles.button}>
+                    <Button title="Login" onPress={handleLogin} color="#6200ee" />
+                </View>
+                <View style={styles.button}>
+                    <Button title="SignUp" onPress={handleSignUp} color="#6200ee" />
+                </View>
+            </View>
             <Snackbar visible={visible} onDismiss={() => setVisible(false)}> Login Failed </Snackbar>
         </View>
     );
@@ -64,18 +86,31 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         backgroundColor: '#fff',
     },
-    button: {
-        backgroundColor: '#6200ee',
-        paddingVertical: 12,
-        borderRadius: 8,
+    passwordContainer: {
+        flexDirection: 'row',
         alignItems: 'center',
+        marginBottom: 16,
     },
-    buttonText: {
-        color: '#fff',
-        fontSize: 18,
-        fontWeight: 'bold',
+    passwordInput: {
+        flex: 1,
+        height: 50,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        paddingHorizontal: 12,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+    },
+    eyeIcon: {
+        padding: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 16,
+    },
+    button: {
+        width: '48%',
     },
 });
-
 
 export default LoginScreen;
