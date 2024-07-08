9b36caa0170221842916fe11a80b5940dae153f5
miki-hershcovitz <mickyh@mail.tau.ac.il>
2024-07-08 10:19:24 +0300

    add metadata

--- backend/GetRoutes/__init__.py
+++ backend/GetRoutes/__init__.py
@@ -16,7 +16,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse("Internal Server Error", status_code=500)
 
         # Connect to the table
-        table_client = TableClient.from_connection_string(connection_string, table_name='RoutesCordinations')
+        metadata_table = TableClient.from_connection_string(connection_string, table_name='RoutesMetadata')
+
+        # Connect to the table
+        route_table = TableClient.from_connection_string(connection_string, table_name='RoutesCordinations')
 
         # Get partition key from request parameters
         partition_key = req.params.get('partitionKey', "Tel Aviv")
@@ -24,12 +27,23 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse("PartitionKey is required.", status_code=400)
 
         # Query the table for all entities with the specified PartitionKey
-        entities = table_client.query_entities(f"PartitionKey eq '{partition_key}'")
+        entities = route_table.query_entities(f"PartitionKey eq '{partition_key}'")
+        # Query the metadata table for all entities with the specified PartitionKey
+        metadata_entities = list(metadata_table.query_entities(f"PartitionKey eq '{partition_key}'"))
+
+        # Convert the metadata entities to a dictionary for quick lookup
+        metadata_dict = {entity['RowKey']: entity for entity in metadata_entities}
 
         # Collect the entities in a list
         results = []
         for entity in entities:
             parsed_entity = parse_entity(dict(entity))
+
+            # Get the metadata for the same RowKey
+            metadata_entity = metadata_dict.get(parsed_entity.get('row_key'))
+            if metadata_entity:
+                parsed_entity.update(metadata_entity)
+
             results.append(parsed_entity)
 
         # Convert the results to JSON
@@ -44,4 +58,5 @@ def parse_entity(entity: Dict) -> Dict:
     parsed_dict = {}
     parsed_dict["start"] = {"latitude": entity.get("start_cord_latitude"), "longitude": entity.get("start_cord_longitude")}
     parsed_dict["end"] = {"latitude": entity.get("end_cord_latitude"), "longitude": entity.get("end_cord_longitude")}
+    parsed_dict["row_key"] = entity.get("RowKey")
     return parsed_dict
