33f044cb956935489076e4c3dd167d82aeb936bf
miki-hershcovitz <mickyh@mail.tau.ac.il>
2024-07-15 15:02:35 +0300

    logsss

--- backend/CollectCoordination/__init__.py
+++ backend/CollectCoordination/__init__.py
@@ -21,11 +21,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         except ValueError:
             return func.HttpResponse("Invalid JSON in request body", status_code=400)
 
-        city = req_body.get('partition_key', 'Tel Aviv')
+        partition_key = req_body.get('partition_key', 'Tel Aviv')
         index = req_body.get('index')
         finish_status = req_body.get('finish_status')
         data = req_body.get('data')
         logging.info(f"request from front is {req_body}")
+        logging.info(f"partition_key is {partition_key}")
 
         if index is None or data is None:
             return func.HttpResponse("Name, index, and data are required.", status_code=400)
@@ -53,34 +54,41 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         # Add or update the route entity in RoutesCordinations
         if index == 0:
             route_entity = {
-                'PartitionKey': city,
+                'PartitionKey': partition_key,
                 'RowKey': name,
                 'start_cord_latitude': latitude,
                 'start_cord_longitude': longitude
             }
+            logging.info(f"start route_table with {route_entity}")
             route_table.create_entity(entity=route_entity)
+            logging.info(f"end route_table with {route_entity}")
             # Also create the initial entity in AllRouteCoordinations
-            coord_entity = {'PartitionKey': city, 'RowKey': name}
+            coord_entity = {'PartitionKey': partition_key, 'RowKey': name}
             coord_entity[f'Coord{index}_Lat'] = latitude
             coord_entity[f'Coord{index}_Lon'] = longitude
+            logging.info(f"start coord_entity update with {coord_entity}")
             coord_table.create_entity(entity=coord_entity)
+            logging.info(f"end coord_entity update with {coord_entity}")
         else:
             if finish_status:
                 try:
-                    route_entity = route_table.get_entity(partition_key=city, row_key=name)
+                    logging.info(f"start route_table after finish with {partition_key} and {name}")
+                    route_entity = route_table.get_entity(partition_key=partition_key, row_key=name)
                     route_entity['end_cord_latitude'] = latitude
                     route_entity['end_cord_longitude'] = longitude
+                    logging.info(f"start update route_table after finish with {partition_key} and {name}")
                     route_table.update_entity(entity=route_entity, mode=UpdateMode.REPLACE)
+                    logging.info(f"finish update route_table after finish with {partition_key} and {name}")
                 except Exception as e:
                     logging.error(f"Error updating route entity: {e}")
                     return func.HttpResponse("Error updating route entity", status_code=500)
 
             # Add coordinates to AllRouteCoordinations
             try:
-                coord_entity = coord_table.get_entity(partition_key=city, row_key=name)
+                coord_entity = coord_table.get_entity(partition_key=partition_key, row_key=name)
             except:
                 coord_entity = {
-                    'PartitionKey': city,
+                    'PartitionKey': partition_key,
                     'RowKey': name
                 }
 
@@ -88,7 +96,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             coord_entity[f'Coord{index}_Lon'] = longitude
             coord_table.upsert_entity(entity=coord_entity)
 
-        return func.HttpResponse(json.dumps({"row_key": name, "partition_key": city}), status_code=200, mimetype="application/json")
+        return func.HttpResponse(json.dumps({"row_key": name, "partition_key": partition_key}), status_code=200, mimetype="application/json")
 
     except Exception as e:
         logging.error(f"Error processing the request: {e}")
