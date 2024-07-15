087323a510acbda61d0e71d5b653857207ab8f04
miki-hershcovitz <mickyh@mail.tau.ac.il>
2024-07-15 15:11:24 +0300

    fix

--- backend/CollectCoordination/__init__.py
+++ backend/CollectCoordination/__init__.py
@@ -21,7 +21,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         except ValueError:
             return func.HttpResponse("Invalid JSON in request body", status_code=400)
 
-        partition_key = req_body.get('partition_key', 'Tel Aviv')
+        partition_key = req_body.get('partition_key')
+        if not partition_key:
+            partition_key = "Tel Aviv"
         index = req_body.get('index')
         finish_status = req_body.get('finish_status')
         data = req_body.get('data')
